bac9bfe2664e9f5621d0c91134705a53362fcefd
SunitaMaurya2051999 <70470207+SunitaMaurya2051999@users.noreply.github.com>
2020-09-08 08:11:53 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,68 @@
+//Home
+function myHom() {
+	alert("Process is Still in Development.");
+}
+
+// Upload
+function myUp() {
+	alert("Upload Module is Still in Development Process.");
+}
+
+//Gellery
+function myGel() {
+	alert(" Gellery Module is Still in Development Process.");
+}
+
+// Contect
+function myCon() {
+	alert("Conetct Module is Still in Development Process.");
+}
+
+// Sign up
+function mySign() {
+	alert("Sign Up Module is Still in Development Process.");
+}
+
+// Logout
+function myLog() {
+	alert(" Logout Module is Still in Development Process.");
+}
+
+//About
+function myAb() {
+	alert("About Module is Still in Development Process.");
+}
+
+
+
+
+//Footer Sunita Maurya
+
+function myBio() {
+		alert("Maurya Sunita Whose Design This Page.");
+}
+
+// Linked In
+function myLink() {
+	alert("Contect:-https://www.linkedin.com/in/sunitamaurya1999/");
+}
+
+//FaceBook
+function myFb() {
+	alert("Currently Not Using.\nContect Email:sunitamaurya7520@gmail.com");
+}
+
+//Twitter
+function myTwt() {
+	alert("Currently Not Using.\nContect Email:sunitamaurya7520@gmail.com");
+}
+
+// Pirtrest
+function myPin() {
+	alert("Currently Not Using.\nContect Email:sunitamaurya7520@gmail.com");
+}
+
+//Filnker
+function myFli() {
+	alert("Currently Not Using.\nContect Email:sunitamaurya7520@gmail.com");
+}
